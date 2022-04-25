2faffab21a72c0d08617aabbb13f44f31a6e3b64
Duncan Krebs <dkrebs@krebsnet.com>
2022-04-25 07:15:09 -0500

    Added get nodes web service

--- projects/dunkware-net-cluster-server/src/main/java/com/dunkware/net/cluster/server/web/ClusterWebService.java
+++ projects/dunkware-net-cluster-server/src/main/java/com/dunkware/net/cluster/server/web/ClusterWebService.java
@@ -1,8 +1,41 @@
 package com.dunkware.net.cluster.server.web;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dunkware.net.cluster.json.cluster.ClusterNodeSpec;
+import com.dunkware.net.cluster.json.node.ClusterNodeStats;
+import com.dunkware.net.cluster.server.core.ClusterNode;
+import com.dunkware.net.cluster.server.core.ClusterService;
+
+@RestController
 public class ClusterWebService {
 	
-	// get stats 
+	@Autowired
+	private ClusterService clusterService;
+	
+	@GetMapping(path = "/info/nodes/stats") 
+	public @ResponseBody()List<ClusterNodeSpec> nodesSPecs() {
+		List<ClusterNodeSpec> specs = new ArrayList<ClusterNodeSpec>();
+		Collection<ClusterNode> nodes = clusterService.getNodeService().getNodes();
+		for (ClusterNode clusterNode : nodes) {
+			ClusterNodeSpec spec = new ClusterNodeSpec();
+			spec.setId(clusterNode.getStats().getId());
+			spec.setState(clusterNode.getState());
+			spec.setStatus(clusterNode.getStats());
+			spec.setType(clusterNode.getStats().getType());
+			specs.add(spec);
+		}
+		return specs; 
+				
+		
+	}
 	
 
 }
