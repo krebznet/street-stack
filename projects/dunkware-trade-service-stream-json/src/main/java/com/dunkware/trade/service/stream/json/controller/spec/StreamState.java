bd45f6e80fae3d70bead82e64878d5e1833034dc
Duncan Krebs <dkrebs@krebsnet.com>
2022-04-25 07:19:19 -0500

    PendingStarting added in case of stream session exception

--- projects/dunkware-trade-service-stream-json/src/main/java/com/dunkware/trade/service/stream/json/controller/spec/StreamState.java
+++ projects/dunkware-trade-service-stream-json/src/main/java/com/dunkware/trade/service/stream/json/controller/spec/StreamState.java
@@ -1,6 +1,6 @@
 package com.dunkware.trade.service.stream.json.controller.spec;
 
 public enum StreamState {
-Starting,Running,Stopping,Stopped,Exception
+Starting,Running,Stopping,Stopped,Exception,PendingStarting
 
 }
