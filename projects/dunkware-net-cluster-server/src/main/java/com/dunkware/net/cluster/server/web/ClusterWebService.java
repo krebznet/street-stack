6c85fd632de0ec9d9be13f39bb3985a955458b98
Duncan Krebs <dkrebs@krebsnet.com>
2022-05-02 00:21:54 -0500

    Added logging

--- projects/dunkware-net-cluster-server/src/main/java/com/dunkware/net/cluster/server/web/ClusterWebService.java
+++ projects/dunkware-net-cluster-server/src/main/java/com/dunkware/net/cluster/server/web/ClusterWebService.java
@@ -4,21 +4,31 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dunkware.net.cluster.json.cluster.ClusterNodeSpec;
-import com.dunkware.net.cluster.json.node.ClusterNodeStats;
 import com.dunkware.net.cluster.server.core.ClusterNode;
 import com.dunkware.net.cluster.server.core.ClusterService;
 
 @RestController
 public class ClusterWebService {
 	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private ClusterService clusterService;
+	
+	@PostConstruct
+	private void load() { 
+		logger.info("Starting Celebration Cluster Web Service");
+	}
 	
 	@GetMapping(path = "/info/nodes/stats") 
 	public @ResponseBody()List<ClusterNodeSpec> nodesSPecs() {
