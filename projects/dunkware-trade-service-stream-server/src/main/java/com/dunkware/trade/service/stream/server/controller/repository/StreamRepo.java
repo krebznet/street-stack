b5acf1e2ffd29e288bf65bb50dc0e7da490c71f4
Duncan Krebs <dkrebs@krebsnet.com>
2022-05-02 05:13:30 -0500

    added @Repository tag

--- projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/controller/repository/StreamRepo.java
+++ projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/controller/repository/StreamRepo.java
@@ -1,7 +1,9 @@
 package com.dunkware.trade.service.stream.server.controller.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamRepo extends PagingAndSortingRepository<StreamDO, Long> {
 	
 	// make this easier? 
