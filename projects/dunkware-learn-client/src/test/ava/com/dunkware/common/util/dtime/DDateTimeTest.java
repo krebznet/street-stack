433e2d0bf499a4671289d8c5eeea883575d43829
Duncan Krebs <dkrebs@krebsnet.com>
2022-05-02 12:50:20 -0500

    onward.

--- projects/dunkware-learn-client/src/test/ava/com/dunkware/common/util/dtime/DDateTimeTest.java
+++ projects/dunkware-learn-client/src/test/ava/com/dunkware/common/util/dtime/DDateTimeTest.java
@@ -5,7 +5,7 @@ import com.dunkware.common.util.dtime.DDateTime;
 public class DDateTimeTest {
 
 	public static void main(String[] args) {
-		DDateTime dt = DDateTime.now();
+		DDateTime dt = DDateTime.now(); 
 		System.out.println(dt.get().toString());
 	}
 }
