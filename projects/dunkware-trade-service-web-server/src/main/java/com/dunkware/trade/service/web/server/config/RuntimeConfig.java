dc8d0979f2bcb4b2f80064e01aec0afd85674bf8
Duncan Krebs <dkrebs@krebsnet.com>
2022-06-06 15:10:54 -0500

    fuck you always need a build close

--- projects/dunkware-trade-service-web-server/src/main/java/com/dunkware/trade/service/web/server/config/RuntimeConfig.java
+++ projects/dunkware-trade-service-web-server/src/main/java/com/dunkware/trade/service/web/server/config/RuntimeConfig.java
@@ -2,7 +2,6 @@ package com.dunkware.trade.service.web.server.config;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.dunkware.common.util.executor.DExecutor;
@@ -10,17 +9,7 @@ import com.dunkware.common.util.executor.DExecutor;
 @Service
 public class RuntimeConfig {
 
-	@Value("${mongo.url}")
-	private String mongoURL; 
-	
-	@Value("${mongo.db}") 
-	private String mongoDatabase; 
-	
-	@Value("${service.stream.url.http}")
-	private String serviceStreamHTTP; 
-	
-	@Value("${service.stream.url.grpc}")
-	private String serviceStreamGRPC;
+
 	
 	private DExecutor executor; 
 	
@@ -33,21 +22,7 @@ public class RuntimeConfig {
 		return executor;
 	}
 	
-	public String mongoURL() {
-		return mongoURL;
-	}
-
-	public String mongoDatabase() {
-		return mongoDatabase;
-	}
-
-	public String serviceStreamHTTP() {
-		return serviceStreamHTTP;
-	}
-
-	public String serviceStreamGRPC() {
-		return serviceStreamGRPC;
-	}
+	
 
 	
 	
