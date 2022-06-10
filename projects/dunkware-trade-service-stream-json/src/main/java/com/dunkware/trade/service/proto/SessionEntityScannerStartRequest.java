ab1c7ab19645b9e35a5ad01e693f01fb06fc7d35
Duncan Krebs <dkrebs@krebsnet.com>
2022-06-09 23:40:56 -0500

    XStream Json Model Fixes.

---
+++ projects/dunkware-trade-service-stream-json/src/main/java/com/dunkware/trade/service/proto/SessionEntityScannerStartRequest.java
@@ -0,0 +1,21 @@
+package com.dunkware.trade.service.proto;
+
+import com.dunkware.xstream.net.model.search.SessionEntitySearch;
+
+public class SessionEntityScannerStartRequest {
+	
+	private SessionEntitySearch search;
+
+	public SessionEntitySearch getSearch() {
+		return search;
+	}
+
+	public void setSearch(SessionEntitySearch search) {
+		this.search = search;
+	}
+	
+	
+	
+	
+
+}
