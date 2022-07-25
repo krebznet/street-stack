aff0775c66f13af0986040576dd4681ab88e5073
Duncan Krebs <dkrebs@krebsnet.com>
2022-07-25 00:42:44 -0500

    added cross origins

--- projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/resources/StreamResourceWebService.java
+++ projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/resources/StreamResourceWebService.java
@@ -3,6 +3,7 @@ package com.dunkware.trade.service.stream.server.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dunkware.xstream.model.scanner.SessionEntityScanner;
 
 @RestController
+@CrossOrigin(origins = "*") 
 public class StreamResourceWebService {
 
 	@Autowired
