17547b27a299e7426ea43e91eecc1eb7f3248a15
Duncan Krebs <dkrebs@krebsnet.com>
2022-08-22 00:00:16 -0500

    removed 2 configs and removed profiles

--- projects/dunkware-trade-service-web-server/src/main/java/com/dunkware/trade/service/web/server/config/SpringCloudGatewayRoutingTest.java
+++ projects/dunkware-trade-service-web-server/src/main/java/com/dunkware/trade/service/web/server/config/SpringCloudGatewayRoutingTest.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("TestRock")
 public class SpringCloudGatewayRoutingTest {
 
 	/*
