2bd128be4270fb55d88a8aac5dfe118ad3c945f7
Duncan Krebs <dkrebs@krebsnet.com>
2022-11-17 21:04:30 -0600

    test

--- projects/dunkware-trade-service-beach-server/src/main/java/com/dunkware/trade/service/beach/server/web/BeachWebService.java
+++ projects/dunkware-trade-service-beach-server/src/main/java/com/dunkware/trade/service/beach/server/web/BeachWebService.java
@@ -1,6 +1,14 @@
 package com.dunkware.trade.service.beach.server.web;
 
+/**
+ * Okay here we need streaming shit 
+ * @author duncankrebs
+ *
+ */
 public interface BeachWebService {
 
+	
 	public BeachWebDash getDash();
+	
+	
 }
