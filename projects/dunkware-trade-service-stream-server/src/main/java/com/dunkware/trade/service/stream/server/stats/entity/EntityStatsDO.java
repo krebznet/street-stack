4d24f2173b0c708c739dc78b13ae150b0e2258f8
Duncan Krebs <dkrebs@krebsnet.com>
2023-01-16 18:17:29 -0600

    okay build for me please

--- projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/stats/entity/EntityStatsDO.java
+++ projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/stats/entity/EntityStatsDO.java
@@ -6,9 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "entity_stats")
 public class EntityStatsDO {
 	
 	
--- projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/stats/entity/EntityStatsRepo.java
+++ projects/dunkware-trade-service-stream-server/src/main/java/com/dunkware/trade/service/stream/server/stats/entity/EntityStatsRepo.java
@@ -1,8 +1,7 @@
 package com.dunkware.trade.service.stream.server.stats.entity;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EntityStatsRepo extends MongoRepository<EntityStatsDO, Long> {
+public interface EntityStatsRepo  {
 	
 	
 
