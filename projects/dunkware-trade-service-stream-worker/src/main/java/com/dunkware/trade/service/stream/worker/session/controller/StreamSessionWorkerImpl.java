ef5620eea16b47f11cbeeacaf21afbee57b07781
Duncan Krebs <dkrebs@krebsnet.com>
2023-01-20 23:15:16 -0600

    fuckman

--- projects/dunkware-trade-service-stream-worker/src/main/java/com/dunkware/trade/service/stream/worker/session/controller/StreamSessionWorkerImpl.java
+++ projects/dunkware-trade-service-stream-worker/src/main/java/com/dunkware/trade/service/stream/worker/session/controller/StreamSessionWorkerImpl.java
@@ -40,10 +40,6 @@ public class StreamSessionWorkerImpl implements StreamSessionWorker {
 	
 	private StreamSessionWorkerStartReq startReq;
 	
-	
-
-	
-	
 	private StatusPublisher statusPublisher;
 	
 	public StreamSessionWorkerImpl() {
--- projects/dunkware-trade-service-stream-worker/src/main/java/com/dunkware/trade/service/stream/worker/session/extensions/StreamWorkerEntityScanner.java
+++ projects/dunkware-trade-service-stream-worker/src/main/java/com/dunkware/trade/service/stream/worker/session/extensions/StreamWorkerEntityScanner.java
@@ -1,5 +1,10 @@
 package com.dunkware.trade.service.stream.worker.session.extensions;
 
 public class StreamWorkerEntityScanner {
+	
+	
+	// public void scanner request 
+	// it will get a channel sent to it 
+	
 
 }
