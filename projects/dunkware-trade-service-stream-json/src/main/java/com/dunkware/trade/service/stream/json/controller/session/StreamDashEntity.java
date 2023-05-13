5063c59dbc7f767814db1f0ce83524a915051821
Duncan Krebs <dkrebs@krebsnet.com>
2023-05-12 23:05:37 -0500

    StreamDashEntity

---
+++ projects/dunkware-trade-service-stream-json/src/main/java/com/dunkware/trade/service/stream/json/controller/session/StreamDashEntity.java
@@ -0,0 +1,42 @@
+package com.dunkware.trade.service.stream.json.controller.session;
+
+public class StreamDashEntity {
+
+	private String symbol; 
+	private String name; 
+	private Number last; 
+	private Number volume; 
+	private Number tradeCount;
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Number getLast() {
+		return last;
+	}
+	public void setLast(Number last) {
+		this.last = last;
+	}
+	public Number getVolume() {
+		return volume;
+	}
+	public void setVolume(Number volume) {
+		this.volume = volume;
+	}
+	public Number getTradeCount() {
+		return tradeCount;
+	}
+	public void setTradeCount(Number tradeCount) {
+		this.tradeCount = tradeCount;
+	} 
+	
+	
+}
