f56f390b4f44832e847a978bcf1ef9abcdadb770
Duncan Krebs <dkrebs@krebsnet.com>
2023-05-14 02:01:03 -0500

    Tada

--- projects/dunkware-trade-service-beach-protocol/src/main/java/com/dunkware/trade/service/beach/protocol/broker/AddBrokerResp.java
+++ projects/dunkware-trade-service-beach-protocol/src/main/java/com/dunkware/trade/service/beach/protocol/broker/AddBrokerResp.java
@@ -1,9 +1,22 @@
 package com.dunkware.trade.service.beach.protocol.broker;
 
+import com.dunkware.common.util.json.DJson;
+
 public class AddBrokerResp {
 	
 	private boolean ok = true; 
 	private String error = null;
+	
+	public static void main(String[] args) {
+		AddBrokerResp resp = new AddBrokerResp();
+		resp.setError("Connection Exception Host 13.23.132 Port 23 - Connection Timeout");
+		resp.setOk(false);
+		try {
+			System.out.println(DJson.serializePretty(resp));
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 	
 	public boolean isOk() {
 		return ok;
