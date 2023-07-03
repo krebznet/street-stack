22b9c1654fb02690d63be138adcad31ba43eeece
Duncan Krebs <dkrebs@krebsnet.com>
2023-07-02 23:01:43 -0500

    deplloy updates

--- deploy/deploy-gateway.sh
+++ deploy/deploy-gateway.sh
@@ -1,10 +1,10 @@
 echo "Executing Maven Build"
 cd /Users/duncankrebs/dunkware/street/cloud/1.0.0/dunkware-street-cloud
 
-mvn clean install  -DskipTests
+#mvn clean install  -DskipTests
 
 echo "Deploying Gateway Service"
 cd /Users/duncankrebs/dunkware/street/cloud/1.0.0/dunkware-street-cloud/projects/dunkware-trade-service-web-server
 docker build --platform=linux/amd64 -t dunkware-trade-service-web-server:latest .
 docker tag dunkware-trade-service-web-server:latest testrock1.dunkware.net:31000/dunkware-trade-service-web-server:latest
-docker push testrock1.dunkware.net:31000/dunkware-trade-service-web-server:latest
+docker -D  push  testrock1.dunkware.net:31000/dunkware-trade-service-web-server:latest
