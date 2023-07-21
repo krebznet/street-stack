580e7e6847ab32afaadef109e6456729f3755e6e
Duncan Krebs <dkrebs@krebsnet.com>
2023-07-21 03:12:47 -0500

    updating apis for beans

--- projects/dunkware-trade-sdk-core-runtime/src/main/java/com/dunkware/trade/sdk/core/runtime/broker/BrokerAccount.java
+++ projects/dunkware-trade-sdk-core-runtime/src/main/java/com/dunkware/trade/sdk/core/runtime/broker/BrokerAccount.java
@@ -1,15 +1,14 @@
 package com.dunkware.trade.sdk.core.runtime.broker;
 
 import com.dunkware.common.util.events.DEventNode;
-import com.dunkware.trade.sdk.core.model.broker.BrokerAccountSpec;
+import com.dunkware.trade.sdk.core.model.broker.AccountBean;
+import com.dunkware.trade.sdk.core.model.broker.AccountStatus;
 import com.dunkware.trade.sdk.core.model.order.OrderType;
 import com.dunkware.trade.sdk.core.runtime.order.Order;
 import com.dunkware.trade.sdk.core.runtime.order.OrderException;
 import com.dunkware.trade.sdk.core.runtime.order.OrderPreview;
 
 public interface BrokerAccount {
-	
-	BrokerAccountSpec getSpec();
 	
 	Order createOrder(OrderType type) throws OrderException;
 	
@@ -20,6 +19,16 @@ public interface BrokerAccount {
 	DEventNode getEventNode();
 	
 	Broker getBroker();
+	
+	AccountBean getBean();
+	
+	AccountStatus getStatus();
+	
+	void init();
+	
+	void dispose();
+	
+	
 	
 
 }
