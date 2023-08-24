4be718d16450932a9bedddf450d634e3f9ddd944
Duncan Krebs <dkrebs@krebsnet.com>
2023-08-24 14:08:08 -0500

    fuck me, spring test.

---
+++ projects/dunkware-spring-test-client/src/main/java/com/dunkware/trade/service/web/server/model/User.java
@@ -0,0 +1,79 @@
+package com.dunkware.trade.service.web.server.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+	
+    Long userId;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public LocalDateTime getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public void setUpdatedOn(LocalDateTime updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+}
