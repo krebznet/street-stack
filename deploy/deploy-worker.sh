e69aa7b23ca8b5466e93f8321232070ac891fa5f
Duncan Krebs <dkrebs@krebsnet.com>
2023-08-31 03:26:24 -0500

    Fixed deployments

--- deploy/deploy-worker.sh
+++ deploy/deploy-worker.sh
@@ -3,5 +3,5 @@
 echo "Deploying Worker Service"
 cd /Users/duncankrebs/dunkware/street/cloud/1.0.0/dunkware-street-cloud/projects/dunkware-trade-service-worker-server
 docker build --platform=linux/amd64 -t dunkware-trade-service-worker-server:latest .
-docker tag dunkware-trade-service-worker-server:latest testrock1.dunkware.net:31000/dunkware-trade-service-worker-server:latest
-docker push testrock1.dunkware.net:31000/dunkware-trade-service-worker-server:latest
+docker tag dunkware-trade-service-worker-server:latest testrock1:31000/dunkware-trade-service-worker-server:latest
+docker push testrock1:31000/dunkware-trade-service-worker-server:latest
